4b29623a1a1c4a41754ef2086a6d42c7480c5100
Hanzou Urushihara <61415393+im-hanzou@users.noreply.github.com>
2024-11-14 16:23:05 +0700

    Update run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import cloudscraper
 from loguru import logger
 
 def show_warning():
-    confirm = input("By using this tool means you understand the risks. do it at your own risk! \nPress Enter to continue or Ctrl+C to cancel... ")
+    confirm = input("Nodepay Autofarmer by IM-Hanzou: github.com/im-hanzou\n\nBy using this tool means you understand the risks. do it at your own risk! \nPress Enter to continue or Ctrl+C to cancel... ")
 
     if confirm.strip() == "":
         print("Continuing...")

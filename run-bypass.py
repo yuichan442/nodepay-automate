8c38f7075239eecb627f8133442896936dc54ba6
Hanzou Urushihara <61415393+im-hanzou@users.noreply.github.com>
2024-11-19 15:30:15 +0700

    Update run-bypass.py

--- run-bypass.py
+++ run-bypass.py
@@ -34,9 +34,10 @@ DOMAIN_API_ENDPOINTS = {
         "http://api.nodepay.ai/api/auth/session"
     ],
     "PING": [
-        "http://54.255.192.166/api/network/ping",
+        #"http://54.255.192.166/api/network/ping",
         "http://52.77.10.116/api/network/ping",
-        "http://13.215.134.222/api/network/ping"
+        "http://13.215.134.222/api/network/ping",
+        "http://52.74.35.173/api/network/ping"
     ]
 }
 

1893338e370951d26024b2ae1daa7c5f5c7a8d06
Hanzou Urushihara <61415393+im-hanzou@users.noreply.github.com>
2024-11-20 16:04:28 +0700

    Update run-multi-bypass.py

--- run-multi-bypass.py
+++ run-multi-bypass.py
@@ -27,9 +27,6 @@ DOMAIN_API_ENDPOINTS = {
     "PING": [
         "http://13.215.134.222/api/network/ping",
         "http://18.139.20.49/api/network/ping",
-        "http://18.142.29.174/api/network/ping",
-        "http://18.142.214.13/api/network/ping",
-        "http://52.74.31.107/api/network/ping",
         "http://52.74.35.173/api/network/ping",
         "http://52.77.10.116/api/network/ping"
     ]

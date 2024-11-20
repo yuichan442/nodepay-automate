0cb8cac760e3724adef7e0158f39bbbcb31e0b29
Hanzou Urushihara <61415393+im-hanzou@users.noreply.github.com>
2024-11-20 18:39:31 +0700

    Update run-bypass.py

--- run-bypass.py
+++ run-bypass.py
@@ -37,7 +37,7 @@ DOMAIN_API_ENDPOINTS = {
         "http://18.139.20.49/api/network/ping",
         "http://52.74.35.173/api/network/ping",
         "http://52.77.10.116/api/network/ping",
-        "http://3.1.154.253/api/network/ping
+        "http://3.1.154.253/api/network/ping"
     ]
 }
 
